1f0574992c5dcf82ff44c6434d525e1578622c31
Hashir Irfan <62703507+sheikh-hashir@users.noreply.github.com>
2020-07-31 18:54:48 +0500

    commit without networking

--- Chess_1/src/King.java
+++ Chess_1/src/King.java
@@ -58,11 +58,190 @@ public class King extends Piece
                         }
                     }
                 }
-     
+                System.out.println("i= "+i);System.out.println("j= "+j);
             }
         }
         return possibleMoves;
     }
+    public boolean isBownVerticalDanger(Cell [][] squares)
+    {
+        int i=this.x+1,j=this.y;
+        while(i<=7)
+        {
+            if(checkDanger(squares, i, j)==true)
+            {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+    public boolean isUpVerticalDanger(Cell [][] squares)
+    {
+        int i=this.x-1,j=this.y;
+        while(i>=0)
+        {
+            if(checkDanger(squares, i, j)==true)
+            {
+                return true;
+            }
+            i--;
+        }
+        return false;
+    }
+    public boolean isLeftHorizontalDanger(Cell [][] squares)
+    {
+        int i=this.x,j=this.y-1;
+        while(j>=0)
+        {
+            if(checkDanger(squares, i, j)==true)
+                return true;
+            j--;
+        }
+        return false;
+    }
+    public boolean isRightHorizontalDanger(Cell [][] squares)
+    {
+        int i=this.x,j=this.y+1;
+        while(j<=7)
+        {
+            if(checkDanger(squares, i, j)==true)
+            {
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+    public boolean checkDanger(Cell [][] squares, int i, int j)
+    {
+        if(squares[i][j].getPiece()==null)  return false;
+        else if(squares[i][j].getPiece()!=null && squares[i][j].getPiece().getColor()!=this.getColor())
+        {
+            if(squares[i][j].getPiece() instanceof Rook || squares[i][j].getPiece() instanceof Queen)
+                return true;
+            else
+                return false;
+        }
+        else if(squares[i][j].getPiece().getColor() == getColor())
+            return false;
+        return false;
+    }
+    
+    /*diagonals*/
+    public boolean isPrimaryDiagonalDanger(Cell [][] squares)
+    {
+        int i=this.x-1,j=this.y-1;
+        while(j>=0 && i>=0)
+        {
+            if(checkDangerDiagonal(squares, i, j)==true)
+            {
+                System.out.println("1");
+                return true;
+            }
+            j--;i--;
+        }
+        
+        i=this.x+1;j=this.y+1;
+        while(j<=7 && i<=7)
+        {
+            if(checkDangerDiagonal(squares, i, j)==true)
+            {
+                return true;
+            }
+            j++;i++;
+        }        
+        return false;
+    }
+    public boolean isSecondaryDiagonalDanger(Cell [][] squares)
+    {
+        int i=this.x-1,j=this.y+1;
+        while(i>=0 && j<=7)
+        {
+            if(checkDangerDiagonal(squares, i, j)==true)
+            {
+                return true;
+            }
+            j++;i--;
+        }
+        
+        i=this.x+1;j=this.y-1;
+        while(j>=0 && i<=7)
+        {
+            if(checkDangerDiagonal(squares, i, j)==true)
+            {
+                return true;
+            }
+            j--;i++;
+        }        
+        return false;
+    }
+    public boolean checkDangerDiagonal(Cell [][] squares, int i, int j)
+    {
+        System.out.println("i= "+i+" j= "+j);
+        if(squares[i][j].getPiece()==null) { return false; }
+        else if(squares[i][j].getPiece()!=null && squares[i][j].getPiece().getColor()!=this.getColor())
+        {
+            if(squares[i][j].getPiece() instanceof Bishop || squares[i][j].getPiece() instanceof Queen)
+                return true;
+            else
+                return false;
+        }
+        else if(squares[i][j].getPiece().getColor() == getColor())
+            return false;
+        return false;
+    }
+    public boolean isKnightDanger(Cell [][] squares)
+    {
+        int posx[]={x+1,x+1,x+2,x+2,x-1,x-1,x-2,x-2};
+        int posy[]={y-2,y+2,y-1,y+1,y-2,y+2,y-1,y+1};
+        for(int i=0;i<8;i++)
+        {
+            if((posx[i]>=0&&posx[i]<8&&posy[i]>=0&&posy[i]<8))
+            {
+                if(squares[posx[i]][posy[i]].getPiece()!=null && squares[posx[i]][posy[i]].getPiece().getColor()!=this.getColor() && (squares[posx[i]][posy[i]].getPiece() instanceof Knight))
+                {
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean isPawnDanger(Cell [][] squares)
+    {
+		//Checking for attack from the Pawn of opposite color
+        int pox[]={x+1,x+1,x+1,x,x,x-1,x-1,x-1};
+        int poy[]={y-1,y+1,y,y+1,y-1,y+1,y-1,y};
+        {
+            for(int i=0;i<8;i++)
+            {
+                if((pox[i]>=0&&pox[i]<8&&poy[i]>=0&&poy[i]<8))
+                {
+                    if(squares[pox[i]][poy[i]].getPiece()!=null && squares[pox[i]][poy[i]].getPiece().getColor()!=this.getColor() && (squares[pox[i]][poy[i]].getPiece() instanceof King))
+                    {
+                            return true;
+                    }
+                }
+            }
+        }
+        if(getColor()==0)
+        {
+            if(x>0 && y>0 && squares[x-1][y-1].getPiece()!=null && squares[x-1][y-1].getPiece().getColor()==1 && (squares[x-1][y-1].getPiece() instanceof Pawn))
+                    return true;
+            if(x>0 && y<7 && squares[x-1][y+1].getPiece()!=null && squares[x-1][y+1].getPiece().getColor()==1 && (squares[x-1][y+1].getPiece() instanceof Pawn))
+                    return true;
+        }
+        else
+        {
+            if(x<7 && y>0 && squares[x+1][y-1].getPiece()!=null && squares[x+1][y-1].getPiece().getColor()==0 && (squares[x+1][y-1].getPiece() instanceof Pawn))
+                    return true;
+            if(x<7 && y<7 && squares[x+1][y+1].getPiece()!=null && squares[x+1][y+1].getPiece().getColor()==0 && (squares[x+1][y+1].getPiece() instanceof Pawn))
+                    return true;
+        }
+        return false;
+    }
+
+    
     
     
     
